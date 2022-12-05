6f0f0f2125153feb1cda4d660aff74bcc9eed61d
usuario <usuario@192.168.0.157>
2022-12-05 14:24:19 -0300

    Renombrado de Test Collections

--- Test Suites/Suite Collection de Pruebas.ts
+++ Test Suites/Suite Collection de Pruebas.ts
@@ -1,7 +1,7 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <TestSuiteCollectionEntity>
    <description></description>
-   <name>New Test Suite Collection</name>
+   <name>Suite Collection de Pruebas</name>
    <tag></tag>
    <delayBetweenInstances>0</delayBetweenInstances>
    <executionMode>SEQUENTIAL</executionMode>
